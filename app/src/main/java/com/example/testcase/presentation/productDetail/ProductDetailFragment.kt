9974b4eece7ed0dacba730d26521c36956b5daa1
burak selçuk <89164849+burakselcuk1@users.noreply.github.com>
2024-02-14 21:49:34 +0300

    Added add to basket button

--- app/src/main/java/com/example/testcase/presentation/productDetail/ProductDetailFragment.kt
+++ app/src/main/java/com/example/testcase/presentation/productDetail/ProductDetailFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.base.core.BaseFragment
 import com.bumptech.glide.Glide
 import com.example.testcase.R
+import com.example.testcase.data.local.DatabaseModel
 import com.example.testcase.databinding.FragmentProductDetailBinding
 import com.example.testcase.databinding.FragmentProductMainFragmentBinding
 import com.example.testcase.presentation.productMainFragment.model.ProductUiModel
@@ -31,9 +33,20 @@ class ProductDetailFragment : BaseFragment<FragmentProductDetailBinding,ProductD
                 Glide.with(productDetailImage.context)
                     .load(product.image)
                     .into(productDetailImage)
+
+                addToBasket.setOnClickListener {
+                    Toast.makeText(context, "${getString(R.string.added_to_db)}  ${product.name}", Toast.LENGTH_SHORT).show()
+                    saveToDb(product)
+                }
             }
         }
         setListeners()
+    }
+
+    private fun saveToDb(product: ProductUiModel) {
+        val dbProduct = DatabaseModel(product.id.toInt(),product.model,product.name,product.image,product.price,
+            product.description,product.model,product.brand,product.createdAt)
+        viewModel.insertToDb(dbProduct)
     }
 
     private fun setListeners() {
--- app/src/main/java/com/example/testcase/presentation/productDetail/ProductDetailViewModel.kt
+++ app/src/main/java/com/example/testcase/presentation/productDetail/ProductDetailViewModel.kt
@@ -1,6 +1,30 @@
 package com.example.testcase.presentation.productDetail
 
+import androidx.lifecycle.viewModelScope
 import com.base.core.BaseViewModel
+import com.example.testcase.common.Resource
+import com.example.testcase.data.local.DatabaseModel
+import com.example.testcase.presentation.basket.BasketUseCase
+import com.example.testcase.presentation.basket.model.BasketUiModel
+import com.example.testcase.presentation.productMainFragment.ProductMainUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ProductDetailViewModel: BaseViewModel() {
+@HiltViewModel
+class ProductDetailViewModel @Inject constructor(
+    private val productDbUseCase: BasketUseCase
+): BaseViewModel() {
+
+    private val _insertResult = MutableStateFlow<Resource<BasketUiModel>>(Resource.Loading)
+    val insertResult: MutableStateFlow<Resource<BasketUiModel>> = _insertResult
+
+    fun insertToDb(product: DatabaseModel) {
+        viewModelScope.launch {
+            productDbUseCase.insertToDb(product).collect { result ->
+                _insertResult.value = result
+            }
+        }
+    }
 }
